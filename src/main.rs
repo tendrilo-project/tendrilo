53e1429496d95d1894da37bb659a31832e87fa95
kciryt <tyrick@gmail.com>
2018-02-04 22:01:50 -0700

    trying out a fluent-style serializer, and cleanup

--- src/main.rs
+++ src/main.rs
@@ -5,191 +5,65 @@ extern crate hex;
 extern crate byteorder;
 
 use chrono::{ Utc };
-//use std::io::prelude::*;
-use std::net::{TcpStream, TcpListener};
-//use std::io::{Cursor, Seek, Write};
+use std::net::{TcpStream};
+use std::io::{Write, Read};
 
 mod util;
 use util::crypto::{CryptoUtils};
-
-//use byteorder::{BigEndian, LittleEndian, NativeEndian, NetworkEndian, ReadBytesExt, WriteBytesExt};
 
 mod serializer;
 use serializer::{Serializer, ByteOrder};
 
 fn main() {
-    println!("Bitcoin play!");
 
-    let mut serializer : Serializer = Serializer::new();
-    serializer = serializer.push_i32(70015, ByteOrder::LittleEndian);
-    serializer = serializer.push_byte(1);
-    serializer = serializer.push(&serializer.get_stream());
-    serializer.print();
+    let version : Serializer = Serializer::create_named("Version")
+        .fluent_i32_push(70015, ByteOrder::LittleEndian)
+        .fluent_u64_push(0, ByteOrder::LittleEndian)
+        .fluent_i64_push(Utc::now().timestamp() as i64, ByteOrder::LittleEndian)
 
-    serializer.push_byte_mut(1);
-    serializer.push_byte_mut(0);
-    serializer.push_byte_mut(1);
-    serializer.push_byte_mut(0);
-    serializer.print();
+        //recv
+        .fluent_u64_push(0, ByteOrder::LittleEndian)
+        .fluent_buffer_push(&vec![ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x7F, 0x00, 0x00, 0x01 ])
+        .fluent_u16_push(8333, ByteOrder::NetworkEndian)
 
+        //trans
+        .fluent_u64_push(0, ByteOrder::LittleEndian)
+        .fluent_buffer_push(&vec![ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x7F, 0x00, 0x00, 0x00 ])
+        .fluent_u16_push(8333, ByteOrder::NetworkEndian)
 
-    serializer = serializer.push_byte_fluent(2)
-        .push_byte_fluent(2)
-        .push_byte_fluent(2)
+        .fluent_buffer_push(&vec![0x3B, 0x2E, 0xB3, 0x5D, 0x8C, 0xE6, 0x17, 0x65]) //nounce
+        .fluent_u64_push(0, ByteOrder::LittleEndian)
+        .fluent_i32_push(507360, ByteOrder::LittleEndian)
+        .fluent_byte_push(0)
         .build();
-    serializer.print();
-    //let mut buffer = Cursor::new(vec![]);
 
+    version.dump();
+
+    println!("");
+    //raw_version.extend_from_slice(b"::ffff:127.0.0.1");
+    //raw_version.extend_from_slice(b"107.191.33.83\0\0\0");
+
+    let checksum = CryptoUtils::sha256(&CryptoUtils::sha256(&version.get_stream()));
+
+    let message : Serializer = Serializer::create_named("Message")
+        .fluent_u32_push(0xD9B4BEF9 as u32, ByteOrder::LittleEndian)
+        .fluent_static_buffer_push(b"version\0\0\0\0\0")
+        .fluent_u32_push(version.get_stream().len() as u32, ByteOrder::LittleEndian)
+        .fluent_byte_push(checksum[3])
+        .fluent_byte_push(checksum[2])
+        .fluent_byte_push(checksum[1])
+        .fluent_byte_push(checksum[0])
+        .fluent_buffer_push(&version.get_stream())
+        .build();
+
+    message.dump();
 
     /*
-    let mut test = vec![];
-    test.extend_from_slice(&serialize_i32(70015, ByteOrder::LittleEndian));
-    test.extend_from_slice(&serialize_i32(70015, ByteOrder::BigEndian));
-
-    println!("{}", to_hex_string(&test));
-
-    let mut raw_version = vec![];
-    raw_version.write_i32::<LittleEndian>(70015).unwrap();
-    raw_version.write_u64::<LittleEndian>(0).unwrap();
-    raw_version.write_i64::<LittleEndian>(Utc::now().timestamp() as i64).unwrap();
-    raw_version.write_u64::<LittleEndian>(0).unwrap();
-    raw_version.extend_from_slice(&vec![ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x7F, 0x00, 0x00, 0x01 ]);
-    //raw_version.extend_from_slice(b"::ffff:127.0.0.1");
-    raw_version.write_u16::<NetworkEndian>(8333).unwrap();
-    raw_version.write_u64::<BigEndian>(0).unwrap();
-
-    //raw_version.extend_from_slice(b"107.191.33.83\0\0\0");
-    raw_version.extend_from_slice(&vec![ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00 ]);
-
-    raw_version.write_u16::<NetworkEndian>(8333).unwrap();
-    //raw_version.write_u64::<LittleEndian>(23445).unwrap();
-    raw_version.extend_from_slice(&vec![0x3B, 0x2E, 0xB3, 0x5D, 0x8C, 0xE6, 0x17, 0x65]);
-
-    raw_version.write_u64::<LittleEndian>(0).unwrap();
-    raw_version.write_i32::<LittleEndian>(507360).unwrap();
-    raw_version.push(0);
-
-    println!("raw_version hex content:");
-    println!("{}", to_hex_string(&raw_version));
-
-/*
-    let version : Version = Version {
-        version: 70015,
-        services: 0x00,
-        timestamp: Utc::now().timestamp() as i64,
-        addr_recv_services: 0,
-        addr_recv_ip_address: *b"::ffff:127.0.0.1",
-        add_recv_port: 8333,
-        addr_trans_services: 0,
-        addr_trans_ip_address: *b"107.191.33.83\0\0\0",
-        addr_trans_port: 8333,
-        nonce: 0, //random
-        user_agent_bytes: 0,
-        start_height: 507360,
-        relay: false
-    };
-*/
-    println!("");println!("");println!("");println!("");println!("");
-
-    let checksum = crypto_utils::CryptoUtils::sha256(&crypto_utils::CryptoUtils::sha256(&raw_version));
-
-/*
-    let message : Message = Message {
-        magic: 0xD9B4BEF9 as u32,
-        command: *b"version\0\0\0\0\0",
-        length: mem::size_of::<Version>() as u32,
-        checksum: 34, //u32
-        payload: version
-    };
-*/
-    println!("raw_version length is {}", raw_version.len());
-    let mut raw_message = vec![];
-    raw_message.write_u32::<LittleEndian>(0xD9B4BEF9 as u32).unwrap();
-    raw_message.extend_from_slice(b"version\0\0\0\0\0");
-    raw_message.write_u32::<LittleEndian>(raw_version.len() as u32).unwrap();
-    raw_message.push(checksum[3]);
-    raw_message.push(checksum[2]);
-    raw_message.push(checksum[1]);
-    raw_message.push(checksum[0]);
-    raw_message.extend_from_slice(&raw_version);
-
-    println!("raw_message hex content:");
-    println!("{}", to_hex_string(&raw_message));
-
-/*
-F9 BE B4 D9
-76 65 72 73 69 6F 6E 00 00 00 00 00
-00 00 00 5D (X)
-09 F8 B3 8F
-
-
-
-
-
-00 01 11 7F (X)
-00 00 00 00 00 00 00 01 (X)
-
-00 00 00 00 5A 76 2E 36 (X)
-
-00 00 00 00 00 00 00 00 3A 3A 66 66 66 66 3A 31 32 37 2E 30 2E 30 2E 31 20 8D
-00 00 00 00 00 00 00 00 31 30 37 2E 31 39 31 2E 33 33 2E 38 33 00 00 00 20 8D
-
-00 00 00 00 00 00 09 29
-
-00 00 00 00 00 00 00 00
-
-00 07 BD E0 (X)
-
-00
-*/
-        let test_messsage = vec![
-             // magic number
-             0xF9, 0xBE, 0xB4, 0xD9,
-             // command
-             0x76, 0x65, 0x72, 0x73, 0x69, 0x6F, 0x6E, 0x00, 0x00, 0x00,
-             0x00, 0x00,
-             // message length
-             0x65, 0x00, 0x00, 0x00,
-             // checksum
-             0x03, 0x0E, 0xCC, 0x57,
-
-             // version
-             0x62, 0xEA, 0x00, 0x00,
-             // services
-             0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-             // timestamp
-             0x11, 0xB2, 0xD0, 0x50, 0x00, 0x00, 0x00, 0x00,
-
-
-
-             // addr recv
-             0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, //service
-
-             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, //address
-
-             0x00, 0x00, //port
-
-
-
-             // addr from
-             0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF,
-             0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-             // nonce
-             0x3B, 0x2E, 0xB3, 0x5D, 0x8C, 0xE6, 0x17, 0x65,
-             // user-agent
-             0x0F, 0x2F, 0x53, 0x61, 0x74, 0x6F, 0x73, 0x68, 0x69, 0x3A,
-             0x30, 0x2E, 0x37, 0x2E, 0x32, 0x2F,
-             // last block height
-             0xC0, 0x3E, 0x03, 0x00,
-             // relay
-    0x00];
-
     {
-        //let listener = TcpListener::bind("127.0.0.1:8333").unwrap();
-        //let mut stream = TcpStream::connect("52.14.94.187:8333").unwrap();
-        //let _ = stream.write(&raw_message);
-        //let _ = stream.read(&mut [0; 128]); // ignore here too\
-    } // the stream is closed here
-*/
+        let mut stream = TcpStream::connect("52.14.94.187:8333").unwrap();
+        let _ = stream.write(&message.get_stream());
+        let _ = stream.read(&mut [0; 128]); // ignore here too\
+    }
+    */
+    // the stream is closed here
 }
--- src/serializer/mod.rs
+++ src/serializer/mod.rs
@@ -1,9 +1,8 @@
 extern crate bincode;
 extern crate hex;
 extern crate byteorder;
-
-use std::io::{Cursor, Seek, Write};
 use byteorder::{BigEndian, LittleEndian, NativeEndian, NetworkEndian, ReadBytesExt, WriteBytesExt};
+use std::string::{String};
 
 pub enum ByteOrder {
     BigEndian,
@@ -13,6 +12,7 @@ pub enum ByteOrder {
 }
 
 pub struct Serializer {
+    name: &'static str,
     data: Vec<u8>
 }
 
@@ -24,71 +24,102 @@ pub fn to_hex_string(bytes: &Vec<u8>) -> String {
 }
 
 impl Serializer {
-    pub fn new() -> Serializer {
+
+    pub fn create() -> Serializer {
         Serializer {
+            name: "Serializer",
             data: vec![]
         }
     }
 
-    pub fn push_byte(&self, buffer: u8) -> Serializer {
-        let mut new_data = self.data.clone();
-        new_data.push(buffer);
+    pub fn create_named(name: &'static str) -> Serializer {
         Serializer {
-            data: new_data
+            name: name,
+            data: vec![]
         }
     }
 
+    // Mutable operations
     pub fn push_byte_mut(&mut self, buffer: u8) {
         self.data.push(buffer);
     }
 
-    pub fn push_byte_fluent(mut self, buffer: u8) -> Serializer {
+    // Fluent operations
+
+    pub fn fluent_byte_push(mut self, buffer: u8) -> Serializer {
         self.data.push(buffer);
+        self
+    }
+
+    pub fn fluent_buffer_push(mut self, buffer: &Vec<u8>) -> Serializer {
+        self.data.extend_from_slice(buffer);
+        self
+    }
+
+    pub fn fluent_static_buffer_push(mut self, buffer: &'static [u8]) -> Serializer {
+        self.data.extend_from_slice(buffer);
+        self
+    }
+
+    pub fn fluent_u16_push(mut self, int: u16, byteorder: ByteOrder) -> Serializer {
+        match byteorder {
+             ByteOrder::LittleEndian => self.data.write_u16::<LittleEndian>(int).unwrap(),
+             ByteOrder::BigEndian => self.data.write_u16::<BigEndian>(int).unwrap(),
+             ByteOrder::NativeEndian => self.data.write_u16::<NativeEndian>(int).unwrap(),
+             ByteOrder::NetworkEndian => self.data.write_u16::<NetworkEndian>(int).unwrap()
+        }
+        self
+    }
+
+    pub fn fluent_i32_push(mut self, int: i32, byteorder: ByteOrder) -> Serializer {
+        match byteorder {
+             ByteOrder::LittleEndian => self.data.write_i32::<LittleEndian>(int).unwrap(),
+             ByteOrder::BigEndian => self.data.write_i32::<BigEndian>(int).unwrap(),
+             ByteOrder::NativeEndian => self.data.write_i32::<NativeEndian>(int).unwrap(),
+             ByteOrder::NetworkEndian => self.data.write_i32::<NetworkEndian>(int).unwrap()
+        }
+        self
+    }
+
+    pub fn fluent_u32_push(mut self, int: u32, byteorder: ByteOrder) -> Serializer {
+        match byteorder {
+             ByteOrder::LittleEndian => self.data.write_u32::<LittleEndian>(int).unwrap(),
+             ByteOrder::BigEndian => self.data.write_u32::<BigEndian>(int).unwrap(),
+             ByteOrder::NativeEndian => self.data.write_u32::<NativeEndian>(int).unwrap(),
+             ByteOrder::NetworkEndian => self.data.write_u32::<NetworkEndian>(int).unwrap()
+        }
+        self
+    }
+
+    pub fn fluent_u64_push(mut self, int: u64, byteorder: ByteOrder) -> Serializer {
+        match byteorder {
+             ByteOrder::LittleEndian => self.data.write_u64::<LittleEndian>(int).unwrap(),
+             ByteOrder::BigEndian => self.data.write_u64::<BigEndian>(int).unwrap(),
+             ByteOrder::NativeEndian => self.data.write_u64::<NativeEndian>(int).unwrap(),
+             ByteOrder::NetworkEndian => self.data.write_u64::<NetworkEndian>(int).unwrap()
+        }
+        self
+    }
+
+    pub fn fluent_i64_push(mut self, int: i64, byteorder: ByteOrder) -> Serializer {
+        match byteorder {
+             ByteOrder::LittleEndian => self.data.write_i64::<LittleEndian>(int).unwrap(),
+             ByteOrder::BigEndian => self.data.write_i64::<BigEndian>(int).unwrap(),
+             ByteOrder::NativeEndian => self.data.write_i64::<NativeEndian>(int).unwrap(),
+             ByteOrder::NetworkEndian => self.data.write_i64::<NetworkEndian>(int).unwrap()
+        }
         self
     }
 
     pub fn build(&self) -> Serializer {
         Serializer {
+            name: self.name,
             data: self.data.clone()
         }
     }
 
-    pub fn push(&self, buffer: &Vec<u8>) -> Serializer {
-        let mut new_data = self.data.clone();
-        new_data.extend_from_slice(buffer);
-        Serializer {
-            data: new_data
-        }
-    }
-
-    pub fn push_i32(&self, int: i32, byteorder: ByteOrder) -> Serializer {
-        let mut new_data = self.data.clone();
-        match byteorder {
-             ByteOrder::LittleEndian => new_data.write_i32::<LittleEndian>(int).unwrap(),
-             ByteOrder::BigEndian => new_data.write_i32::<BigEndian>(int).unwrap(),
-             ByteOrder::NativeEndian => new_data.write_i32::<NativeEndian>(int).unwrap(),
-             ByteOrder::NetworkEndian => new_data.write_i32::<NetworkEndian>(int).unwrap()
-        }
-        Serializer {
-            data: new_data
-        }
-    }
-
-    pub fn fluent_push_i32(&self, int: i32, byteorder: ByteOrder) -> Serializer {
-        let mut new_data = self.data.clone();
-        match byteorder {
-             ByteOrder::LittleEndian => new_data.write_i32::<LittleEndian>(int).unwrap(),
-             ByteOrder::BigEndian => new_data.write_i32::<BigEndian>(int).unwrap(),
-             ByteOrder::NativeEndian => new_data.write_i32::<NativeEndian>(int).unwrap(),
-             ByteOrder::NetworkEndian => new_data.write_i32::<NetworkEndian>(int).unwrap()
-        }
-        Serializer {
-            data: new_data
-        }
-    }
-
-    pub fn print(&self) {
-        println!("{}", to_hex_string(&self.data));
+    pub fn dump(&self) {
+        println!("{} hex dump:\n{}", self.name, to_hex_string(&self.data));
     }
 
     pub fn get_stream(&self) -> Vec<u8> {
